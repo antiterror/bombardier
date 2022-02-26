998a16763692920951dc677f389d0da294f08248
yarik.rachkov <d>
2022-02-26 19:37:35 +0200

    remove comments

--- src/main.py
+++ src/main.py
@@ -27,11 +27,9 @@ def main_process(site):
 
     name = str(site).replace('.','_').replace('http://','').replace('https://','').rstrip('/')
     print(name)
-  #  try:
-
 
     docker.containers.run(image=image_name,
-                          command=f' -c 1000 -d 3600s -l {site}',
+                          command=f' -c 1 -d 3s -l {site}',
                           name=f"{name}",
                           detach=False
                           )
@@ -39,15 +37,11 @@ def main_process(site):
     output_log = container.logs().decode("utf-8")
     print(output_log)
     return None
-    # except:
-    #     output_log = "Unexpected error:", sys.exc_info()[0]
-    #     print(output_log)
-    #     return None
+
 
 
 def get_list_sites():
     # clean and compact list
-    print(sites)
     site_list_all = [line.strip() for line in str(sites).split(" ") if line.strip()]
     print(site_list_all)
     return site_list_all
